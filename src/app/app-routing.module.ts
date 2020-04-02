a040a0dabe7a8c5c0a487b563eaa9542944d3920
Sameekshan <sameekshan@confluxtechnologies.com>
2020-04-02 18:49:28 +0530

    Crud operations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+import { MemberGetComponent } from './member-get/member-get.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'member/create',
+    component: MemberAddComponent
+  },
+  {
+    path: 'edit/:id',
+    component: MemberEditComponent
+  },
+  {
+    path: 'members',
+    component: MemberGetComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberGetComponent } from './member-get/member-get.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { MemberService } from './member.service';
+
+import { ReactiveFormsModule } from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberAddComponent,
+    MemberGetComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  
+ 
+  providers: [ MemberService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
